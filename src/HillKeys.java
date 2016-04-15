cdf68da5a693689749b6c50dbe81ccb574f8df74
Emil Lengman <emillen@kth.se>
2016-04-15 14:58:13 +0200

    generateK creates an array mod 26

--- src/HillKeys.java
+++ src/HillKeys.java
@@ -1,42 +1,64 @@
-import org.jscience.mathematics.vector.Float64Matrix;
+import org.jscience.mathematics.number.Real;
+import org.jscience.mathematics.vector.*;
+
+import java.util.Random;
 
 /**
- *  A program that generates a matrix K, and its inverse
- *  to be used as Hill cipher keys
- *  @author Emil Lengman
- *  @author Simon Enerstrand
+ * A program that generates a matrix K, and its inverse
+ * to be used as Hill cipher keys
+ *
+ * @author Emil Lengman
+ * @author Simon Enerstrand
  */
 public class HillKeys {
 
     public static void main(String[] args) {
 
-        if (args.length != 2) {
+        /*if (args.length != 2) {
             System.out.println("Usage: HillKeys <file>");
             return;
-        }
-        String fileName = args[1];
-        Float64Matrix K = generateK();
-        Float64Matrix inverse = K.inverse();
-        writeToFile(fileName, K, inverse);
+        }*/
+        //String fileName = args[1];
+        DenseMatrix<Real> K = generateK();
+        System.out.println(K.toString()); // testing if it works
+        Matrix<Real> inverse = K.inverse();
+        /*writeToFile(fileName, K, inverse);*/
     }
 
-
     /**
-     * Generates a invertible matrix
+     * Generates an invertible matrix
+     *
      * @return a invertible matrix
      */
-    private static Float64Matrix generateK() {
+    private static DenseMatrix<Real> generateK() {
         // TODO Generate a invertible matrix
-        return null;
+        DenseMatrix<Real> K;
+        Random r = new Random();
+
+        while (true) {
+            Real[][] arr = {
+                    {Real.valueOf(r.nextInt(26)), Real.valueOf(r.nextInt(26)), Real.valueOf(r.nextInt(26))},
+                    {Real.valueOf(r.nextInt(26)), Real.valueOf(r.nextInt(26)), Real.valueOf(r.nextInt(26))},
+                    {Real.valueOf(r.nextInt(26)), Real.valueOf(r.nextInt(26)), Real.valueOf(r.nextInt(26))}
+            };
+
+
+            K = DenseMatrix.valueOf(arr);
+            if (K.determinant().doubleValue() != 0)
+                break;
+        }
+
+        return K;
     }
 
     /**
-     * Writes, the matrix, and inverse to a file
-     * @param fileName  the name of the file to write to
-     * @param matrix    the matrix
-     * @param inverse   the inverse
+     * Writes the matrix, and inverse to a file
+     *
+     * @param fileName the name of the file to write to
+     * @param matrix   the matrix
+     * @param inverse  the inverse
      */
-    private static void writeToFile(String fileName, Float64Matrix matrix, Float64Matrix inverse) {
+    private static void writeToFile(String fileName, DenseMatrix matrix, DenseMatrix inverse) {
 
         // TODO Write matrices and inverse to file
 
