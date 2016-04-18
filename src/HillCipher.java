566b5207e04346827fba82c4143078feca29db45
Emil Lengman <emillen@kth.se>
2016-04-18 15:59:31 +0200

    Comments and cleaning up imports

--- src/HillCipher.java
+++ src/HillCipher.java
@@ -1,16 +1,11 @@
 import org.jscience.mathematics.number.Real;
 import org.jscience.mathematics.vector.DenseMatrix;
 import org.jscience.mathematics.vector.DenseVector;
-import org.jscience.mathematics.vector.Matrix;
 
 import java.io.*;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * A program used to encrypt a file using hill cipher algorithm
@@ -52,13 +47,17 @@ public class HillCipher {
         return K.times(message);
     }
 
+    /**
+     * Reads a encryption matrix K from a file
+     *
+     * @param KFileName the name of the file
+     * @return a DenseMatrix
+     */
     private static DenseMatrix<Real> getK(String KFileName) {
 
         if (!(new File(KFileName).isFile()))
             return null;
 
-
-        // TODO read from file, and create DenseMatrix
         List<DenseVector<Real>> vectors = new ArrayList<>();
 
         try (
@@ -78,12 +77,24 @@ public class HillCipher {
         return DenseMatrix.valueOf(vectors);
     }
 
+    /**
+     * Returns a vector that is parsed from a string
+     *
+     * @param line the string to parse
+     * @return a DenseVector that the string represents
+     */
     private static DenseVector<Real> getVector(String line) {
 
         // TODO parse lines to get vector
         return null;
     }
 
+    /**
+     * Creates a matrix from the contents of a file
+     *
+     * @param messageFileName the name of the file
+     * @return a DenseMatrix that represents the content of the file
+     */
     private static DenseMatrix<Real> getMessageMatrix(String messageFileName) {
 
         if (!(new File(messageFileName).isFile()))
