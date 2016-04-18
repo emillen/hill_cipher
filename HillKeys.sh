a177056028e7067f6ae2318ecc777de56e8b1a69
Emil Lengman <emillen@kth.se>
2016-04-18 18:28:38 +0200

    Changed sh file and stuff

--- HillKeys.sh
+++ HillKeys.sh
@@ -3,7 +3,7 @@ if [ -z "$2" ]; then
     echo 'Usage: HillKeys <fileName>'
 else
     mkdir -p out
-    javac -cp 'lib/jscience.jar' -d 'out/' src/HillKeys.java
+    javac -cp 'lib/jscience.jar' -d 'out/' src/HillKeys.java src/Util.java
     java -cp 'lib/jscience.jar:out/' HillKeys $1 $2
 fi
 
