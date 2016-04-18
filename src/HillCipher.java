1673c78c38e69e9612d6f325aa8f3be34312a5e5
Emil Lengman <emillen@kth.se>
2016-04-18 15:55:30 +0200

    Implemented getK() and cipher()

--- src/HillCipher.java
+++ src/HillCipher.java
@@ -1,7 +1,16 @@
 import org.jscience.mathematics.number.Real;
 import org.jscience.mathematics.vector.DenseMatrix;
+import org.jscience.mathematics.vector.DenseVector;
+import org.jscience.mathematics.vector.Matrix;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * A program used to encrypt a file using hill cipher algorithm
@@ -19,38 +28,71 @@ public class HillCipher {
         // TODO write program
         if (args.length != 2) {
 
-            System.out.println("Usage: HillEncrypt <encryptKeyFile> <messageFile>");
+            System.out.println("Usage: HillEncrypt <KFile> <messageFile>");
         }
 
         String encryptFileName = args[0];
         String messageFileName = args[1];
 
-        DenseMatrix<Real> encryptionKey;
+        DenseMatrix<Real> K;
 
-        if((encryptionKey = getEncryptionKey(encryptFileName)) == null) {
+        if ((K = getK(encryptFileName)) == null) {
             System.out.println("File <" + encryptFileName + "> does not exist, or does not contain a matrix");
         }
+        DenseMatrix<Real> messageMatrix;
+        if ((messageMatrix = getMessageMatrix(messageFileName)) == null) {
+            System.out.println("The file does not exist, or does not contain a valid message");
+        }
 
+        System.out.println(cipher(K, messageMatrix).toString());
     }
 
-    private static DenseMatrix<Real> getEncryptionKey(String encryptFileName){
+    private static DenseMatrix<Real> cipher(DenseMatrix<Real> K, DenseMatrix<Real> message) {
 
-        if(!(new File(encryptFileName).isFile()))
+        return K.times(message);
+    }
+
+    private static DenseMatrix<Real> getK(String KFileName) {
+
+        if (!(new File(KFileName).isFile()))
             return null;
 
-        // TODO read from file, and create DenseMatrix
 
+        // TODO read from file, and create DenseMatrix
+        List<DenseVector<Real>> vectors = new ArrayList<>();
+
+        try (
+                InputStream is = new FileInputStream(KFileName);
+                InputStreamReader isr = new InputStreamReader(is, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(isr)
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                vectors.add(getVector(line));
+            }
+        } catch (Exception e) {
+
+            return null;
+        }
+        return DenseMatrix.valueOf(vectors);
+    }
+
+    private static DenseVector<Real> getVector(String line) {
+
+        // TODO parse lines to get vector
         return null;
     }
 
-    private static DenseMatrix<Real> getMessageMatrix(String messageFileName){
+    private static DenseMatrix<Real> getMessageMatrix(String messageFileName) {
 
-        if(!(new File(messageFileName).isFile()))
+        if (!(new File(messageFileName).isFile()))
             return null;
 
         // TODO read text from file, and create DenseMatrix
-
         return null;
 
     }
+
+
 }
