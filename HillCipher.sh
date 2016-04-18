c4ec1f14a7174babad36372c2dfb61ef4291f1ae
Emil Lengman <emillen@kth.se>
2016-04-18 18:29:11 +0200

    Made shell script for HillCipher

---
+++ HillCipher.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [ -z "$3" ]; then
+    echo 'Usage: HillCipher <KFileName> <messageFileName>'
+else
+    mkdir -p out
+    javac -cp 'lib/jscience.jar' -d 'out/' src/HillCipher.java src/Util.java
+    java -cp 'lib/jscience.jar:out/' HillCipher $1 $2 $3
+fi
+
