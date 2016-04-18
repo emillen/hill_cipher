5bd94f97cc81072af20960d112b0472044de2d41
Emil Lengman <emillen@kth.se>
2016-04-18 14:34:48 +0200

    made HillDecrypt and HillEncrypt files

---
+++ src/HillDecrypt.java
@@ -0,0 +1,13 @@
+/**
+ * A program used to decrypt a file using hill cipher algorithm
+ *
+ * @author Emil Lengman
+ * @author Simon Enerstrand
+ */
+public class HillDecrypt {
+
+    public static void main(String[] args) {
+
+        // TODO write program
+    }
+}
---
+++ src/HillEncrypt.java
@@ -0,0 +1,13 @@
+/**
+ * A program used to encrypt a file using hill cipher algorithm
+ *
+ * @author Emil Lengman
+ * @author Simon Enerstrand
+ */
+public class HillEncrypt {
+
+    public static void main(String[] args) {
+
+        // TODO write program
+    }
+}
