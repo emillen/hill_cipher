31a3b355eb2b8b7131b29d455168da90ef23d405
Emil Lengman <emillen@kth.se>
2016-04-18 21:28:43 +0200

    Correct printout

--- HillCipher.sh
+++ HillCipher.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ -z "$3" ]; then
-    echo 'Usage: HillCipher <KFileName> <messageFileName>'
+    echo 'Usage: HillCipher <KFileName> <messageFileName> <saveFileName>'
 else
     mkdir -p out
     javac -cp 'lib/jscience.jar' -d 'out/' src/HillCipher.java src/Util.java
